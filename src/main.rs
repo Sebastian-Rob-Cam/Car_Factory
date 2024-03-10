bdb69a82f7b8d6765adf04dca35c40557a3547af
Sebastian Robles Camacho <sebastian.rob.cam@gmail.com>
2024-03-09 21:41:30 -0600

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,76 @@
+#[derive(PartialEq, Debug)]
+// Declare Car struct to describe vehicle with four named fields
+struct Car {
+    color: String,
+    motor: Transmission,
+    roof: bool,
+    age: (Age, u32),
+}
+
+#[derive(PartialEq, Debug)]
+// Declare enum for Car transmission type
+enum Transmission {
+    Manual,
+    SemiAuto,
+    Automatic,
+}
+
+#[derive(PartialEq, Debug)]
+// Declare enum for Car age
+enum Age {
+    New,
+    Old,
+}
+
+fn car_quality(miles: u32) -> (Age, u32) {
+    let quality: (Age, u32);
+
+    match miles {
+        0 => quality = (Age::New, miles),
+        _ => quality = (Age::Old, miles),
+    }
+
+    quality
+}
+
+fn car_factory(color: String, motor: Transmission, roof: bool, miles: u32) -> Car {
+    Car {
+        color: color,
+        motor: motor,
+        roof: roof,
+        age: car_quality(miles),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let colors = ["Blue", "Green", "Red", "Silver"];
+
+    let mut car: Car;
+
+    let mut engine: Transmission = Transmission::Manual;
+
+    // Order 3 cars, one car for each type of transmission
+
+    // Car order #1: New, Manual, Hard top
+    car = car_factory(String::from(colors[0]), engine, true, 0);
+    println!(
+        "Car order 1: {:?}, Hard top = {}, {:?}, {}, {} miles",
+        car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
+
+    // Car order #2: Used, Semi-automatic, Convertible
+    engine = Transmission::SemiAuto;
+    car = car_factory(String::from(colors[1]), engine, false, 100);
+    println!(
+        "Car order 2: {:?}, Hard top = {}, {:?}, {}, {} miles",
+        car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
+
+    // Car order #3: Used, Automatic, Hard top
+    engine = Transmission::Automatic;
+    car = car_factory(String::from(colors[2]), engine, true, 200);
+    println!(
+        "Car order 3: {:?}, Hard top = {}, {:?}, {}, {} miles",
+        car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
 }
